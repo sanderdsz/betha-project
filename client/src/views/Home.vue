f859f7f8b637153c53935292b01a2e1836047b7a
Sander <sanderdsz@gmail.com>
2021-12-03 08:18:12 -0300

    navigation and strapi connect

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,50 @@
 <template>
   <div class="container">
-    <router-view></router-view>
+    <form v-on:submit.prevent>
+      <input v-model="object.data.message">
+      <button v-on:click="update">Submit</button>
+    </form>
+    <h1>{{ response.message }}</h1>
+    {{ responseAxios }}
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      object: {
+        data: {
+          message: ''
+        }
+      },
+      response: {
+        message: ''
+      }
+    }
+  },
+  methods: {
+    async update() {
+      await axios.put("http://152.70.211.106:8080/api/health", this.object)
+    },
+    async get() {
+      await axios.get("http://152.70.211.106:8080/api/health")
+      .then(response => this.response = response.data.data.attributes)
+    }
+  },
+  computed: {
+    responseAxios: () => {
+      return this.response.message
+    }
+  },
+  created () {
+    this.get()
+  },
+  mounted () {
+  }
 }
 </script>
 
